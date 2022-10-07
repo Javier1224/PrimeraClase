ad3f19d7e31c7749bd3b634bd997a741c56f97d2
Javier_leon <jajole02@hotmail.com>
2022-10-07 15:32:45 -0500

    Primera Pueba

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello world!");
+    }
+    public static int suma(int a, int b)
+    {
+        return a+b;
+    }
+}
+
+class coche
+{
+    public int num=0;
+    public void funssion()
+    {
+
+    }
+}
