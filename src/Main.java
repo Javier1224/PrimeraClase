87ebb4bc6a26d2e003bd2640c523dc8698399721
Javier_leon <jajole02@hotmail.com>
2022-10-07 15:47:11 -0500

    Fin de la tarea

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,28 @@ public class Main
 {
     public static void main(String[] args)
     {
-
-        System.out.println("Hello world!");
+        int resultado=suma(5, 6, 7);
+        System.out.println(resultado);
+        coche renault= new coche();
+        renault.aumentar_puertas();
+        renault.aumentar_puertas();
+        System.out.println(renault.numero_puertas());
     }
-    public static int suma(int a, int b)
+    public static int suma(int a, int b, int c)
     {
-        return a+b;
+        return a+b+c;
     }
 }
 
 class coche
 {
-    public int num=0;
-    public void funssion()
+    public int puertas=0;
+    public void aumentar_puertas()
     {
-
+        puertas++;
+    }
+    public int numero_puertas()
+    {
+        return puertas;
     }
 }
